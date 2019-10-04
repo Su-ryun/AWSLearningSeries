ebd43bfd3df42aa28f42ad7ac22cdc0efbc0e674
Su-ryun <dlee67@msudenver.edu>
2019-10-03 20:44:00 -0600

    AWS and stuff.

---
+++ DayOne/Calc.js
@@ -0,0 +1,45 @@
+console.log('Loading the calculator function');
+
+// So, the below is like the entry point for Lambda to execute your code,
+// where the callback is optional.
+exports.handler = function(event, context, callback) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    if (event.a === undefined || event.b === undefined || event.op === undefined) {
+        callback("400 Invalid Input");
+    }
+    
+    var res = {};
+    res.a = Number(event.a);
+    res.b = Number(event.b);
+    res.op = event.op;
+    
+    if (isNaN(event.a) || isNaN(event.b)) {
+        callback("400 Invalid Operand");
+    }
+
+    switch(event.op)
+    {
+        case "+":
+        case "add":
+            res.c = res.a + res.b;
+            break;
+        case "-":
+        case "sub":
+            res.c = res.a - res.b;
+            break;
+        case "*":
+        case "mul":
+            res.c = res.a * res.b;
+            break;
+        case "/":
+        case "div":
+            res.c = res.b===0 ? NaN : Number(event.a) / Number(event.b);
+            break;
+        default:
+            callback("400 Invalid Operator");
+            break;
+    }
+    //It’s used to return information to the caller in the RequestResponse invocation type, 
+    //such as when a function is invoked via the API Gateway.
+    callback(null, res);
+};
